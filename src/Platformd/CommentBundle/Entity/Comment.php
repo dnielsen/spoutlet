6eb4df5f0d655a2a76d7cd6678b4c2c1011f0f1f
Ryan Weaver <ryan@thatsquality.com>
2011-10-10 13:38:27 -0500

    [Comment] Actually rendering the comments, and hooking up the comments with the User entity. See #12 - this will also affect #16 - see the comments

--- src/Platformd/CommentBundle/Entity/Comment.php
+++ src/Platformd/CommentBundle/Entity/Comment.php
@@ -5,13 +5,16 @@ namespace Platformd\CommentBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use FOS\CommentBundle\Entity\Comment as BaseComment;
 use Platformd\CommentBundle\Entity\Thread;
+use FOS\CommentBundle\Model\SignedCommentInterface;
+use FOS\CommentBundle\Model\VotableCommentInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Table(name="comment")
  * @ORM\Entity
  * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment extends BaseComment implements SignedCommentInterface
 {
     /**
      * @ORM\Id
@@ -24,9 +27,17 @@ class Comment extends BaseComment
      * Thread of this comment
      *
      * @var Thread
-     * @ORM\ManyToOne(targetEntity="Platformd\CommentBundle\Entity\Thread")
+     * @ORM\ManyToOne(targetEntity="Thread")
      */
     protected $thread;
+
+    /**
+     * Author of the comment
+     *
+     * @ORM\ManyToOne(targetEntity="Platformd\UserBundle\Entity\User")
+     * @var User
+     */
+    protected $author;
 
     /**
      * @return Thread
@@ -43,5 +54,32 @@ class Comment extends BaseComment
     public function setThread(Thread $thread)
     {
         $this->thread = $thread;
+    }
+
+    /**
+     * @return \Platformd\UserBundle\Entity\User
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param \Platformd\UserBundle\Entity\User $author
+     */
+    public function setAuthor(UserInterface $author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * Helps the SignedCommentInterface - not needed, but default implementation
+     * is just to return "Anonymous".
+     *
+     * @return string
+     */
+    public function getAuthorName()
+    {
+        return ($this->getAuthor()) ? $this->getAuthor()->getUsername() : parent::getAuthorName();
     }
 }
