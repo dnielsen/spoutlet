4f99bbbe7a1a1a37f20ccf6778fb325d4357e3b7
Romain Pouclet <me@palleas.com>
2011-10-10 17:59:00 +0200

    Allow the user to choose a source he may have heard about alienware arena from

--- src/Platformd/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Platformd/UserBundle/Form/Type/RegistrationFormType.php
@@ -7,6 +7,19 @@ use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 
 class RegistrationFormType extends BaseType
 {
+    /**
+     * @var array source the user may have heard about alienware arena from
+     */
+    private $sources;
+
+    /**
+     * @param string $class The User class name
+     */
+    public function __construct($class, array $sources = array())
+    {
+        parent::__construct($class);
+        $this->sources = $sources;
+    }
     
     public function buildForm(FormBuilder $builder, array $options)
     {
@@ -19,9 +32,11 @@ class RegistrationFormType extends BaseType
             ->add('phoneNumber')
             ->add('country', 'country')
             ->add('state')
-            ->add('hasAlienwareSystem', 'checkbox')
-            ->add('hasAlienwareSystem')
-            ->add('latestNewsSource')
+            ->add('hasAlienwareSystem', 'checkbox', array('required' => false))
+            ->add('latestNewsSource', 'choice', array(
+                'empty_value' => 'Select one',
+                'choices' => $this->sources
+            ))
             ->add('subscribedArenaNews')
             ->add('subscribedGamingNews')
             ->add('termsAccepted', 'checkbox', array('required' => false));
