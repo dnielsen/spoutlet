c1ade2e54fae770f17e2212ccdcfba3c0f82e2d3
Ryan Weaver <ryan@thatsquality.com>
2011-10-10 13:27:42 -0500

    [Comment] Setting our bundle to override the core bundle - overriding protected controller method to redirect back to event page - see #12

---
+++ src/Platformd/CommentBundle/Controller/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Platformd\CommentBundle\Controller;
+
+use FOS\CommentBundle\Controller\CommentController as BaseCommentController;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @author Ryan Weaver <ryan@knplabs.com>
+ */
+class CommentController extends BaseCommentController
+{
+    /**
+     * Overridden so that we can redirect the user back to the event page
+     */
+    protected function onCreateSuccess(Form $form)
+    {
+        // the id we gave to this thread
+        // @TODO - when we change to slugging events, the id should be the slug
+        // and this will need to be updated
+        $threadId = $form->getData()->getThread()->getId();
+        $url = $this->container->get('router')->generate('events_detail', array('id' => $threadId));
+
+        return new RedirectResponse($url);
+    }
+}
