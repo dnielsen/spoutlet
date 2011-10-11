ff439134bbc58a179dcba713e821c465692d435a
Romain Pouclet <me@palleas.com>
2011-10-11 12:12:25 +0200

    Override default user profile form type

---
+++ src/Platformd/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Platformd\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilder;
+
+use FOS\UserBundle\Form\Type\ProfileFormType as BaseProfileFormType;
+
+class ProfileFormType extends BaseProfileFormType
+{
+
+    protected function buildUserForm(FormBuilder $builder, array $options)
+    {
+        parent::buildUserForm($builder, $options);
+
+        $builder
+            ->add('firstname')
+            ->add('lastname');
+    }
+}
