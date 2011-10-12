a94163cc75b71a4b8e9ab0fb9a209ca428fd36ed
Romain Pouclet <me@palleas.com>
2011-10-12 15:01:38 +0200

    User editing

--- src/Platformd/UserBundle/Controller/AdminController.php
+++ src/Platformd/UserBundle/Controller/AdminController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 
+use Platformd\UserBundle\Form\Type\EditUserFormType;
+
 class AdminController extends Controller
 {
     
@@ -20,6 +22,43 @@ class AdminController extends Controller
         
     	return $this->render('UserBundle:Admin:index.html.twig', array(
             'pager' => $pager
+        ));
+    }
+
+    public function editAction($id)
+    {
+        $manager = $this->get('fos_user.user_manager');
+        $translator = $this->get('translator');
+
+        if (!($user = $manager->findUserBy(array('id' => $id))) || $user->isSuperAdmin()) {
+            
+            throw $this->createNotFoundException(sprintf('Unable to retrieve user #%d', $id));
+        }
+
+        $form = $this->createForm(new EditUserFormType(), $user);
+        $request = $this->getRequest();
+
+        // TODO : use update http method
+        if ('POST' === $request->getMethod()) {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                
+                $manager->updateUser($user);
+
+                $request
+                    ->getSession()
+                    ->setFlash('notice', $translator->trans('fos_user_admin_edit_success', array(
+                        '%username%' => $user->getUsername()
+                    ), 'FOSUserBundle'));
+
+                return $this->redirect($this->generateUrl('Platformd_UserBundle_admin_index'));
+            }
+        }
+        
+        return $this->render('UserBundle:Admin:edit.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView()
         ));
     }
     
