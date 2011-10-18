896d989853cc0cf092e478622994b0798f55d949
Geoffrey Bachelet <grosfrais@gmail.com>
2011-10-18 14:33:37 +0200

    added a japanese event in the fixtures

--- src/Platformd/SpoutletBundle/DataFixtures/ORM/02_LoadEventData.php
+++ src/Platformd/SpoutletBundle/DataFixtures/ORM/02_LoadEventData.php
@@ -45,6 +45,23 @@ class LoadEventData extends AbstractFixture implements OrderedFixtureInterface
 
         $manager->persist($event);
 
+        // Current japanese event event
+        $event = new Event();
+        $event->setName('[japan] Portal 2 — Challenge Co-op');
+        $event->setReady(true);
+        $event->setPublished(true);
+        $event->setStartsAt(new \DateTime('october 1'));
+        $event->setEndsat(new \DateTime('november 1'));
+        $event->setCity('Lyon');
+        $event->setCountry('France');
+        $event->setLocation('online');
+        $event->setLocale('ja');
+        $event->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempor est sem, vitae interdum odio. Praesent varius, tortor eget venenatis molestie, dui urna tristique nisl, a tincidunt quam nulla ac nisl. Mauris nulla lacus, lobortis dictum luctus ut, condimentum a sapien. Morbi dignissim, nulla non hendrerit molestie, dolor nisl ultrices ipsum, sit amet pharetra dolor metus vitae eros. Pellentesque ac turpis sapien, id commodo justo. Quisque commodo cursus tristique. Phasellus sit amet sem felis, et hendrerit mauris.');
+        $event->setHostedBy('KnpLabs Lyon');
+        $event->setGame('Portal 2');
+
+        $manager->persist($event);
+
         // Upcoming Event
         $event = new Event();
         $event->setName('Batman Arkham City — Race to 100%');
