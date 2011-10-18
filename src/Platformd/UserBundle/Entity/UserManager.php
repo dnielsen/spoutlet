8d027f697bf7ca0699d91f62661338d78dbef750
Geoffrey Bachelet <grosfrais@gmail.com>
2011-10-18 14:37:56 +0200

    removed useless code in UserManager

--- src/Platformd/UserBundle/Entity/UserManager.php
+++ src/Platformd/UserBundle/Entity/UserManager.php
@@ -34,14 +34,6 @@ class UserManager extends BaseUserManager
             $user->updateAvatar();
         }
 
-        if ($user->getIsOrganizer()) {
-            $user->addRole('ROLE_ORGANIZER');
-        }
-
-        if ($user->getIsSuperAdmin()) {
-            $user->addRole('ROLE_SUPER_ADMIN');
-        }
-
         parent::updateUser($user, $andFlush);
     }
 
