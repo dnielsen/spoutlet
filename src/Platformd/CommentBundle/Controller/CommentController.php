7d30afec8e778c4d5bd7c6387cfeb14129d6cd6e
Romain Pouclet <me@palleas.com>
2011-10-20 16:00:59 +0200

    Closes #48; Delete comments

--- src/Platformd/CommentBundle/Controller/CommentController.php
+++ src/Platformd/CommentBundle/Controller/CommentController.php
@@ -2,6 +2,7 @@
 namespace Platformd\CommentBundle\Controller;
 
 use FOS\CommentBundle\Controller\CommentController as BaseCommentController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -10,6 +11,34 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
  */
 class CommentController extends BaseCommentController
 {
+    
+    public function deleteAction($id) 
+    {
+        $manager = $this->container->get('fos_comment.manager.comment');
+
+        if (!$comment = $manager->findCommentById($id)) {
+            
+            throw new NotFoundHttpException('Comment not found.');
+        }
+        
+        // Not sure if the CommentBundle provides a way to delete comments easily
+        // so we just use the ORM directly
+
+        $url = $this->container->get('router')->generate('events_detail', array('slug' => $comment->getThread()->getId()));
+        
+        $em = $this->container->get('doctrine.orm.entity_manager');
+
+        $thread = $comment->getThread();
+        $thread->setNumComments($thread->getNumComments() - 1);
+        $em->persist($thread);
+
+        $em->remove($comment);
+
+        $em->flush();
+
+        return new RedirectResponse($url);
+    }
+
     /**
      * Overridden so that we can redirect the user back to the event page
      */
