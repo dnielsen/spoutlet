18a37381e714e4f8b9f6dca2ed251c3da3380232
Ryan Weaver <ryan@thatsquality.com>
2011-10-26 23:31:17 -0500

    Fixing up required fields in profile form

--- src/Platformd/UserBundle/Form/Type/ProfileFormType.php
+++ src/Platformd/UserBundle/Form/Type/ProfileFormType.php
@@ -14,22 +14,22 @@ class ProfileFormType extends BaseProfileFormType
         parent::buildUserForm($builder, $options);
 
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('file')
+            ->add('firstname', null, array('required' => true))
+            ->add('lastname', null, array('required' => true))
+            ->add('file', null, array('required' => false))
             // My rig
-            ->add('type')
-            ->add('manufacturer')
-            ->add('operating_system')
-            ->add('cpu')
-            ->add('memory')
-            ->add('video_card')
-            ->add('sound_card')
-            ->add('hard_drive')
-            ->add('headphones')
-            ->add('mouse')
-            ->add('mousepad')
-            ->add('keyboard')
-            ->add('monitor');
+            ->add('type', null, array('required' => false))
+            ->add('manufacturer', null, array('required' => false))
+            ->add('operating_system', null, array('required' => false))
+            ->add('cpu', null, array('required' => false))
+            ->add('memory', null, array('required' => false))
+            ->add('video_card', null, array('required' => false))
+            ->add('sound_card', null, array('required' => false))
+            ->add('hard_drive', null, array('required' => false))
+            ->add('headphones', null, array('required' => false))
+            ->add('mouse', null, array('required' => false))
+            ->add('mousepad', null, array('required' => false))
+            ->add('keyboard', null, array('required' => false))
+            ->add('monitor'), null, array('required' => false);
     }
 }
