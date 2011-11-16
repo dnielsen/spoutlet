62266db1abc4cd91174a8165eb67cd2c49ac4b9f
Romain Pouclet <me@palleas.com>
2011-11-16 10:28:11 +0100

    Clean path resolving for avatar on profile page

--- src/Platformd/UserBundle/AvatarPathResolver.php
+++ src/Platformd/UserBundle/AvatarPathResolver.php
@@ -19,24 +19,55 @@ class AvatarPathResolver implements PathResolver
    */
   private $filesystem;
 
+  /**
+   * @var string
+   */
+  private $bucketName;
+
+  /**
+   * @var \AmazonS3
+   */
+  private $amazonS3;
+
+  /**
+   * @param Gaufrette\Filesystem $filesystem
+   */
   public function __construct(Filesystem $filesystem)
   {
     $this->filesystem = $filesystem; 
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public function getPath($user, array $options)
   {
-    if ($this->filesystem->getAdapter() instanceof AmazonS3) {
+    if ($this->filesystem->getAdapter() instanceof AmazonS3 && $this->amazonS3) {
       
-      return 'https://s3.amazonaws.com/platformd-testing/'.$user->getAvatar(); 
-      //2e7ee12be388fe7e2d0c9d8514c60b7c385eb8af.jpg';
+      return $this->amazonS3->get_object_url($this->bucketName, $user->getAvatar());
     }
 
     return '/uploads/avatars/'.$user->getAvatar();
   }
 
+  /**
+   * Set the bucket where to look for the file
+   *
+   * @param String $name
+   * @param \AmazonS3 $s3
+   */
+  public function setBucket($name, \AmazonS3 $s3) 
+  {
+    $this->bucketName = $name;
+    $this->amazonS3 = $s3;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   public function supports($media, array $options)
   {
+   
     return $media instanceof User;
   }
 
