de14c3dafdcfad084b76078167f0708cc8d4c39e
Romain Pouclet <me@palleas.com>
2011-11-24 17:17:40 +0100

    add HomepageBanner entity

---
+++ src/Platformd/SpoutletBundle/Entity/HomepageBanner.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Platformd\SpoutletBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Platformd\SpoutletBundle\Entity\HomepageBanner
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Platformd\SpoutletBundle\Entity\HomepageBannerRepository")
+ */
+class HomepageBanner
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $locale
+     *
+     * @ORM\Column(name="locale", type="string", length=5)
+     */
+    private $locale;
+
+    /**
+     * @var string $thumb
+     *
+     * @ORM\Column(name="thumb", type="string", length=255)
+     */
+    private $thumb;
+
+    /**
+     * @var string $banner
+     *
+     * @ORM\Column(name="banner", type="string", length=255)
+     */
+    private $banner;
+
+    /**
+     * @var integer $position
+     *
+     * @ORM\Column(name="position", type="integer")
+     */
+    private $position;
+
+    /**
+     * @var string $url
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set locale
+     *
+     * @param string $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * Get locale
+     *
+     * @return string 
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * Set thumb
+     *
+     * @param string $thumb
+     */
+    public function setThumb($thumb)
+    {
+        $this->thumb = $thumb;
+    }
+
+    /**
+     * Get thumb
+     *
+     * @return string 
+     */
+    public function getThumb()
+    {
+        return $this->thumb;
+    }
+
+    /**
+     * Set banner
+     *
+     * @param string $banner
+     */
+    public function setBanner($banner)
+    {
+        $this->banner = $banner;
+    }
+
+    /**
+     * Get banner
+     *
+     * @return string 
+     */
+    public function getBanner()
+    {
+        return $this->banner;
+    }
+
+    /**
+     * Set position
+     *
+     * @param integer $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * Get position
+     *
+     * @return integer 
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
