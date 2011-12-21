f6259e51a7d7daf442b2e8e7ca67f62ec67bea95
Ryan Weaver <ryan@thatsquality.com>
2011-12-20 20:25:09 -0600

    Adding some to-be-implemented code

--- src/Platformd/SpoutletBundle/Resources/public/admin/js/admin.js
+++ src/Platformd/SpoutletBundle/Resources/public/admin/js/admin.js
@@ -2,4 +2,10 @@ jQuery(document).ready(function() {
     jQuery('table.tablesorter').tablesorter();
 
     jQuery('input.date-picker').datepicker();
+
+    jQuery('.sidebar a[href="#"]').click(function() {
+        alert('to be implemented');
+
+        return false;
+    });
 });
