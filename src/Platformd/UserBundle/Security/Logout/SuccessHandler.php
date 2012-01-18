2be7c83a02f7ef28c5e849d9f13c6741d1bec94f
Ryan Weaver <ryan@thatsquality.com>
2012-01-18 11:11:00 -0600

    Removing video cookie on logout so that the video site doesn't keep API'ing to us after logout

--- src/Platformd/UserBundle/Security/Logout/SuccessHandler.php
+++ src/Platformd/UserBundle/Security/Logout/SuccessHandler.php
@@ -6,6 +6,7 @@ use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\HttpFoundation\Cookie;
 
 /**
  * Custom logout success handler so we can redirect to the video site if needed
@@ -22,7 +23,13 @@ class SuccessHandler implements LogoutSuccessHandlerInterface
     public function onLogoutSuccess(Request $request)
     {
         if ($return = $request->query->get('return')) {
-            return new RedirectResponse($return);
+            $response = new RedirectResponse($return);
+
+            // remove cookie so that video site doesn't keep API'ing to us to try to auth
+            $cookie = new Cookie('aw_session', '');
+            $response->headers->setCookie($cookie);
+
+            return $response;
         }
 
         return $this->httpUtils->createRedirectResponse($request, '/');
