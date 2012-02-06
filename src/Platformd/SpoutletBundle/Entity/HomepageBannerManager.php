bb1635aa6d915e06fb10fe96f3785749b85c9812
Ryan Weaver <ryan@thatsquality.com>
2012-02-06 11:16:48 -0600

    Just cleaning up code standards

--- src/Platformd/SpoutletBundle/Entity/HomepageBannerManager.php
+++ src/Platformd/SpoutletBundle/Entity/HomepageBannerManager.php
@@ -12,52 +12,52 @@ use Gaufrette\Filesystem;
 */
 class HomepageBannerManager
 {
-  /**
-   * @var Doctrine\ORM\EntityManager;
-   */
-  private $manager;
+   /**
+    * @var Doctrine\ORM\EntityManager;
+    */
+    private $manager;
 
-  /**
-   * @var Gaufrette\Filesystem
-   */
-  private $filesystem;
+   /**
+    * @var Gaufrette\Filesystem
+    */
+    private $filesystem;
 
-  public function __construct(EntityManager $manager, Filesystem $filesystem) 
-  {
-    $this->manager = $manager;
-    $this->filesystem = $filesystem;
-  }
-
-  public function save(HomepageBanner $banner)
-  {
-    if ($banner->banner_file) {
-      $banner->setBanner($this->upload($banner->banner_file, 'banner'));
+    public function __construct(EntityManager $manager, Filesystem $filesystem)
+    {
+        $this->manager = $manager;
+        $this->filesystem = $filesystem;
     }
 
-    if ($banner->thumb_file) {
-      $banner->setThumb($this->upload($banner->thumb_file, 'thumb'));
+    public function save(HomepageBanner $banner)
+    {
+        if ($banner->banner_file) {
+          $banner->setBanner($this->upload($banner->banner_file, 'banner'));
+        }
+
+        if ($banner->thumb_file) {
+          $banner->setThumb($this->upload($banner->thumb_file, 'thumb'));
+        }
+
+        $this->manager->persist($banner);
+        $this->manager->flush();
     }
 
-    $this->manager->persist($banner);
-    $this->manager->flush();
-  }
+    protected function upload(File $file, $size)
+    {
+        if (!in_array($size, array('banner', 'thumb'))) {
 
-  protected function upload(File $file, $size)
-  {
-    if (!in_array($size, array('banner', 'thumb'))) {
-      
-      throw new \InvalidArgumentException(sprintf('Invalid size "%s"', $size));
+        throw new \InvalidArgumentException(sprintf('Invalid size "%s"', $size));
+        }
+
+        $filename = $this->generateFilename($size, $file);
+        $this->filesystem->write($filename, file_get_contents($file->getPathname()));
+
+        return $filename;
     }
 
-    $filename = $this->generateFilename($size, $file);
-    $this->filesystem->write($filename, file_get_contents($file->getPathname()));
+    protected function generateFilename($size, File $file)
+    {
 
-    return $filename;
-  }
-
-  protected function generateFilename($size, File $file) 
-  {
-
-    return sprintf('%s-%s.%s', $size, sha1(uniqid()), $file->guessExtension());
-  }
+        return sprintf('%s-%s.%s', $size, sha1(uniqid()), $file->guessExtension());
+    }
 }
