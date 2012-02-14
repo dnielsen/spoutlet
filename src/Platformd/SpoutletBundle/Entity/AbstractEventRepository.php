c428f056f6bcfea61e82803c3d90969823b23118
Ryan Weaver <ryan@thatsquality.com>
2012-02-14 12:48:05 -0600

    Moving repo methods to base

--- src/Platformd/SpoutletBundle/Entity/AbstractEventRepository.php
+++ src/Platformd/SpoutletBundle/Entity/AbstractEventRepository.php
@@ -10,4 +10,76 @@ use Doctrine\ORM\EntityRepository;
  */
 class AbstractEventRepository extends EntityRepository
 {
+    /**
+     * Return current events
+     *
+     * @param integer $limit
+     * @return array
+     */
+    public function getCurrentEvents($locale, $limit = null)
+    {
+        $query = $this->getBaseQueryBuilder($locale)
+            ->andWhere('e.ends_at > :cut_off')
+            ->setParameter('cut_off', new \DateTime())
+            ->orderBy('e.starts_at', 'ASC')
+            ->getQuery();
+
+        return $this->addQueryLimit($query, $limit)->getResult();
+    }
+
+    /**
+     * Return past events
+     *
+     * @param integer $limit
+     * @return array
+     */
+    public function getPastEvents($locale, $limit = null)
+    {
+        $query = $this->getBaseQueryBuilder($locale)
+            ->andWhere('e.ends_at < :cut_off')
+            ->setParameter('cut_off', new \DateTime())
+            ->orderBy('e.starts_at', 'ASC')
+            ->getQuery();
+
+        return $this->addQueryLimit($query, $limit)->getResult();
+    }
+
+    /**
+     * Retrieve published events
+     *
+     * @return array
+     */
+    public function findPublished($locale)
+    {
+
+        return $this->findBy(array(
+            'locale'    => $locale,
+            'published' => true
+        ));
+    }
+
+    /**
+     * Return a query builder instance that should be used for frontend request
+     * basically, it only adds a criteria to retrieve only published events
+     *
+     * @param String $alias
+     * @return Doctrine\ORM\QueryBuilder
+     */
+    private function getBaseQueryBuilder($locale, $alias = 'e')
+    {
+
+        return $this->createQueryBuilder($alias)
+            ->where($alias.'.published = 1')
+            ->andWhere($alias.'.locale = :locale')
+            ->setParameter('locale', $locale);
+    }
+
+    private function addQueryLimit(Query $query, $limit)
+    {
+        if  (null === $limit) {
+            return $query;
+        }
+
+        return $query->setMaxResults($limit);
+    }
 }
