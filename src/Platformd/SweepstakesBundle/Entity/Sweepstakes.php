527dc3de56a421d7e10bf9fea6009caf414b5333
Ryan Weaver <ryan@thatsquality.com>
2012-02-14 11:30:25 -0600

    Adding the new fields to Sweeps entity

--- src/Platformd/SweepstakesBundle/Entity/Sweepstakes.php
+++ src/Platformd/SweepstakesBundle/Entity/Sweepstakes.php
@@ -19,4 +19,93 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Sweepstakes extends AbstractEvent
 {
+    /**
+     * A list of countries that are *not* eligible for this sweeps
+     *
+     * @var array
+     * @ORM\Column(type="array")
+     */
+    protected $disallowedCountries = array();
+
+    /**
+     * @var int
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    protected $minimumAgeRequirement = 13;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $officialRules;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text", nullable=true)
+     */
+    protected $liabilityRelease;
+
+    /**
+     * @return array
+     */
+    public function getDisallowedCountries()
+    {
+        return $this->disallowedCountries;
+    }
+
+    /**
+     * @param array $disallowedCountries
+     */
+    public function setDisallowedCountries($disallowedCountries)
+    {
+        $this->disallowedCountries = $disallowedCountries;
+    }
+
+    /**
+     * @return int
+     */
+    public function getMinimumAgeRequirement()
+    {
+        return $this->minimumAgeRequirement;
+    }
+
+    /**
+     * @param int $minimumAgeRequirement
+     */
+    public function setMinimumAgeRequirement($minimumAgeRequirement)
+    {
+        $this->minimumAgeRequirement = $minimumAgeRequirement;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOfficialRules()
+    {
+        return $this->officialRules;
+    }
+
+    /**
+     * @param string $officialRules
+     */
+    public function setOfficialRules($officialRules)
+    {
+        $this->officialRules = $officialRules;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLiabilityRelease()
+    {
+        return $this->liabilityRelease;
+    }
+
+    /**
+     * @param string $liabilityRelease
+     */
+    public function setLiabilityRelease($liabilityRelease)
+    {
+        $this->liabilityRelease = $liabilityRelease;
+    }
 }
