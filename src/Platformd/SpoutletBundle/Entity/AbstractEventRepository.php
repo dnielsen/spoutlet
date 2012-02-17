311b12efb93965e62f33d43d03dab550f459dfc9
Ryan Weaver <ryan@thatsquality.com>
2012-02-17 13:45:00 -0600

    Fixing a bug (was also fixed on the other branch) where current events were ordered backwards

--- src/Platformd/SpoutletBundle/Entity/AbstractEventRepository.php
+++ src/Platformd/SpoutletBundle/Entity/AbstractEventRepository.php
@@ -21,7 +21,7 @@ class AbstractEventRepository extends EntityRepository
         $query = $this->getBaseQueryBuilder($locale)
             ->andWhere('e.ends_at > :cut_off')
             ->setParameter('cut_off', new \DateTime())
-            ->orderBy('e.starts_at', 'ASC')
+            ->orderBy('e.starts_at', 'DESC')
             ->getQuery();
 
         return $this->addQueryLimit($query, $limit)->getResult();
