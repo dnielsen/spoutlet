1488a943c972cdba21ea83ab87804372a57da47b
Ryan Weaver <ryan@thatsquality.com>
2012-02-23 13:06:26 -0600

    Partially backporting commit from cloudcamp so that the current user sees his account nav on his profile page - see #16

--- src/Platformd/UserBundle/Controller/ProfileController.php
+++ src/Platformd/UserBundle/Controller/ProfileController.php
@@ -4,6 +4,7 @@ namespace Platformd\UserBundle\Controller;
 
 use FOS\UserBundle\Controller\ProfileController as BaseProfileController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Overrides the FOS profile controller
@@ -35,4 +36,28 @@ class ProfileController extends BaseProfileController
 
         return $response;
     }
+
+   public function showAction($username = null)
+   {
+       $context = $this->container->get('security.context');
+
+       if ($username) {
+           $manager = $this->container->get('fos_user.user_manager');
+           if (!$user = $manager->findUserByUsername($username)) {
+               throw new NotFoundHttpException(sprintf('Unable to find an user with username "%s"', $username));
+           }
+           $isCurrentUser = false;
+       } else if ($context->isGranted('IS_AUTHENTICATED_FULLY')) {
+           $isCurrentUser = true;
+           $user = $this->container->get('security.context')->getToken()->getUser();
+       } else {
+           throw new NotFoundHttpException();
+       }
+
+       return $this->container->get('templating')
+           ->renderResponse('FOSUserBundle:Profile:show.html.twig', array(
+           'user' => $user,
+           'isCurrentUser' => $isCurrentUser,
+       ));
+   }
 }
