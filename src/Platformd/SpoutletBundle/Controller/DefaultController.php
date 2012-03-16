39a14f31499c3cbeaade365f63fb372c6238e06b
Ryan Weaver <ryan@thatsquality.com>
2012-03-16 15:50:08 -0500

    Making homepage second tab order by created date - see #69

--- src/Platformd/SpoutletBundle/Controller/DefaultController.php
+++ src/Platformd/SpoutletBundle/Controller/DefaultController.php
@@ -24,7 +24,7 @@ class DefaultController extends Controller
             $abstractEvents = $this->getDoctrine()
                 ->getEntityManager()
                 ->getRepository('SpoutletBundle:AbstractEvent')
-                ->getCurrentEvents($this->getLocale())
+                ->getCurrentEventsOrderedByCreated($this->getLocale())
             ;
         }
 
