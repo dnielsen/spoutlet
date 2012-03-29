03ef6d239243374e554413a5023c647afdd2e4ef
Ryan Weaver <ryan@thatsquality.com>
2012-03-29 18:47:49 -0500

    Making the translation updating part of the deploy process

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -27,3 +27,10 @@ set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor", 
 
 # share our database configuration
 set :shared_files,      ["app/config/parameters.ini"]
+
+# After finalizing update - here to update translations
+after "deploy:finalize_update" do
+
+  run "cd #{latest_release} && #{php_bin} #{symfony_console} spoutlet:translations:entity-extract
+
+end
