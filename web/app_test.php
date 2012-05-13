38711e699a3ae4ba705002a9f6b6bcc7c85eb017
InvertedAcceleration <invertedacceleration@gametheworld.com>
2012-05-13 13:51:03 +0100

    Added my development machine's (VM Host) IP to app_test allowed IP collection.

--- web/app_test.php
+++ web/app_test.php
@@ -5,6 +5,7 @@ umask(0000); // This will let the permissions be 0777
 // feel free to remove this, extend it, or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
+    '192.168.1.81',
     '::1',
 ))) {
     header('HTTP/1.0 403 Forbidden');
