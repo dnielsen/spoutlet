86cd6fd993409cd96561b580ad491c2a3f366449
InvertedAcceleration <invertedacceleration@gametheworld.com>
2012-05-16 23:08:41 +0100

    Added the ability to link News articles to games (add & edit)
    See #113

--- src/Platformd/NewsBundle/Features/Context/FeatureContext.php
+++ src/Platformd/NewsBundle/Features/Context/FeatureContext.php
@@ -11,10 +11,50 @@ use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
 
 use Platformd\SpoutletBundle\Features\Context\AbstractFeatureContext;
-
+use Platformd\SpoutletBundle\Entity\Game as Game;
+use Platformd\NewsBundle\Entity\News as News;
 /**
  * Feature context.
  */
 class FeatureContext extends AbstractFeatureContext
 {
+
+    /**
+     * @var News
+     */
+    private $currentNewsArticle;
+
+    /**
+     * @Given /^I am on the edit page for the news story$/
+     */
+    public function iAmOnTheEditPageForTheNewsStory()
+    {
+        $url = $this->getContainer()->get('router')->generate('NewsBundle_admin_edit', array('id' => $this->currentNewsArticle->getId()));
+
+        $this->getSession()->visit($url);
+    }
+
+    /**
+     * @Given /^there is a news item called "([^"]*)"$/
+     */
+    public function thereIsANewsItemCalled($articleTitle)
+    {
+        $em = $this->getEntityManager();
+
+        $article = new News();
+
+        $article->setTitle($articleTitle);
+
+        $article->setBlurb("blurb");
+        $article->setLocale("en");
+        $article->setBody("body");
+        $article->setGame(NULL);
+        $article->setPostedAt(new \DateTime("2001-01-01"));
+
+        $em->persist($article);
+        $em->flush();
+
+        $this->currentNewsArticle = $article;
+    }
+
 }
