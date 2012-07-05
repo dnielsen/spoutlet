be10de613b6f833c6189a564c417b4fe80665c89
playitcool <jefrykland@igamesinc.com>
2012-07-05 10:11:04 -0700

    Removed blurb from news show page

--- web/app_dev.php
+++ web/app_dev.php
@@ -4,6 +4,7 @@ umask(0000); // This will let the permissions be 0777
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+    '192.168.3.19',
     '192.168.1.81',
     '127.0.0.1',
     '::1',
