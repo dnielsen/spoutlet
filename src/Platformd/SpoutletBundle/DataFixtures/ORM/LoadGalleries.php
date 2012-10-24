9939cea758e6eff93661cde538a47d8065b5bb89
playitcool <jefrykland@igamesinc.com>
2012-10-24 16:44:46 -0700

    Fixture added for galleries

---
+++ src/Platformd/SpoutletBundle/DataFixtures/ORM/LoadGalleries.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Platformd\SpoutletBundle\DataFixtures\ORM;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Platformd\SpoutletBundle\Entity\Gallery;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadGallery extends AbstractFixture implements OrderedFixtureInterface
+{
+    private $container;
+
+    public function load($manager)
+    {
+        $gallery1 = new Gallery();
+        $gallery1->setName('Events');
+        $manager->persist($gallery1);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 2;
+    }
+}
