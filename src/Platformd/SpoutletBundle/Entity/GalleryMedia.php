45835a936c466b6ee53cbdad93616725fc45413a
playitcool <jefrykland@igamesinc.com>
2012-10-26 12:15:05 -0700

    Removed GalleryImage and GalleryVideo entities and instead use GalleryMedia to encompass both for MediaGaller

--- src/Platformd/SpoutletBundle/Entity/GalleryMedia.php
+++ src/Platformd/SpoutletBundle/Entity/GalleryMedia.php
@@ -7,6 +7,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Platformd\UserBundle\Entity\User;
 use Platformd\SpoutletBundle\Link\LinkableInterface;
 use Platformd\SpoutletBundle\Model\ReportableContentInterface;
+use Platformd\MediaBundle\Entity\Media;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Doctrine\Common\Collections\ArrayCollection;
 
@@ -17,7 +18,7 @@ use Doctrine\Common\Collections\ArrayCollection;
  * @ORM\Table(name="pd_gallery_media")
  * @ORM\Entity(repositoryClass="Platformd\SpoutletBundle\Entity\GalleryMediaRepository")
  */
-class GalleryMedia
+class GalleryMedia implements LinkableInterface
 {
 
     static private $validCategories = array(
@@ -41,10 +42,69 @@ class GalleryMedia
     private $category;
 
     /**
-     * @ORM\ManyToOne(targetEntity="GalleryImage")
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     * @Assert\NotNull
+     */
+    private $title;
+
+    /**
+     * @var string $description
+     *
+     * @ORM\Column(name="description", type="string", length=512)
+     */
+    private $description;
+
+    /**
+     * The person who created this gallery image
+     *
+     * @var \Platformd\UserBundle\Entity\User
+     * @ORM\ManyToOne(targetEntity="Platformd\UserBundle\Entity\User")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
+    protected $author;
+
+    /**
+     * @var \DateTime $created
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime $updated
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     * @Gedmo\Timestampable(on="update")
+     */
+    protected $updatedAt;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+
+    private $deleted = false;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $deletedReason = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platformd\MediaBundle\Entity\Media", cascade={"remove"})
      * @ORM\JoinColumn(onDelete="SET NULL")
      */
     protected $image = null;
+
+    /**
+     * @var string $youtubeId
+     *
+     * @ORM\Column(name="youtubeId", type="string", length=255)
+     * @Assert\NotNull
+     */
+    private $youtubeId;
 
     /**
      *
@@ -83,6 +143,77 @@ class GalleryMedia
         return $this->category;
     }
 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+
+    public function setAuthor(User $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+    public function getDeletedReason()
+    {
+        return $this->deletedReason;
+    }
+
+    public function setDeletedReason($deletedReason)
+    {
+        $this->deletedReason = $deletedReason;
+    }
+
     public function getImage()
     {
         return $this->image;
@@ -106,5 +237,51 @@ class GalleryMedia
     public static function getValidCategories()
     {
         return self::$validCategories;
+    }
+
+    /**
+     * If there is a set URL that should be used without doing anything else, return it here
+     *
+     * @return string
+     */
+    public function getLinkableOverrideUrl()
+    {
+        return false;
+    }
+
+    /**
+     * Returns the name of the route used to link to this object
+     *
+     * @return string
+     */
+    public function  getLinkableRouteName()
+    {
+        return $this->getCategory() == 'image' ? 'gallery_view_image' : 'gallery_view_video';
+    }
+
+    /**
+     * Returns an array route parameters to link to this object
+     *
+     * @return array
+     */
+    public function  getLinkableRouteParameters()
+    {
+        return array(
+            'id' => $this->getId(),
+        );
+    }
+
+     /**
+     * Used to return the commenting thread id that should be used for this gallery image
+     */
+    public function getThreadId()
+    {
+        if (!$this->getId()) {
+            throw new \LogicException('A gallery media item needs an id before it can have a comment thread');
+        }
+
+        $commentPrefix = $this->getCategory() == 'image' ? 'gallery_image-' : 'gallery_video-';
+
+        return $commentPrefix.$this->getId();
     }
 }
