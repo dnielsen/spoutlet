952fa327e178aa9708a82fdb0b1c6bb2eff2d529
Andrew Finlay <andy.finlay@gametheworld.com>
2012-11-20 00:53:58 +0000

    Fixed missing variables in deploy script

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -26,8 +26,8 @@ set :user,        "ubuntu"
 # branch can be overridden in any of the "stage" files (e.g. beta)
 set :branch,      "master"
 
-role :web,        app1, app2, app3, app4                         # Your HTTP server, Apache/etc
-role :app,        app1, app2, app3, app4                         # This may be the same as your `Web` server
+role :web,        app1, app2, app3, app4, app5, app6, app7, app8, app9, app10                         # Your HTTP server, Apache/etc
+role :app,        app1, app2, app3, app4, app5, app6, app7, app8, app9, app10                         # This may be the same as your `Web` server
 role :db,         app1, :primary => true       # This is where Rails migrations will run
 
 set  :keep_releases,  3
