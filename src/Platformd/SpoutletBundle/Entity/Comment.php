03f2837c0906906d5645619b0efa2808ca5e8bed
Andrew Finlay <andy.finlay@gametheworld.com>
2012-12-17 20:29:17 +0000

    More additions to new thread/comment entities, added abstract/comment voting entities/repositories

---
+++ src/Platformd/SpoutletBundle/Entity/Comment.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace Platformd\SpoutletBundle\Entity;
+
+use Platformd\UserBundle\Entity\User;
+use Platformd\SpoutletBundle\Model\ReportableContentInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Platformd\SpoutletBundle\Entity\Comment
+ *
+ * @ORM\Table(name="commenting_comment")
+ * @ORM\Entity(repositoryClass="Platformd\SpoutletBundle\Entity\CommentRepository")
+ */
+class Comment implements ReportableContentInterface
+{
+    const DELETED_BY_OWNER  = 'by_owner';
+    const DELETED_BY_ADMIN  = 'by_admin';
+    const DELETED_BY_REPORT = 'REPORTED_PENDING_INVESTIGATION';
+    const DELETED_BY_REPORT_ADMIN = 'REPORTED_AND_REMOVED_BY_ADMIN';
+
+    static private $validDeletedReasons = array(
+        self::DELETED_BY_OWNER,
+        self::DELETED_BY_ADMIN,
+        self::DELETED_BY_REPORT,
+        self::DELETED_BY_REPORT_ADMIN,
+    );
+
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platformd\SpoutletBundle\Entity\Thread")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
+    protected $thread;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platformd\SpoutletBundle\Entity\Comment")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
+    protected $parent;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platformd\UserBundle\Entity\User")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
+    protected $author;
+
+    /**
+     * @ORM\Column(name="body", type="text")
+     */
+    protected $body;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    protected $createdAt;
+
+    //protected $votes;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Platformd\SpoutletBundle\Entity\Comment", mappedBy="parent")
+     */
+    protected $replies;
+
+    /**
+     * @ORM\Column(name="deleted", type="boolean")
+     */
+
+    private $deleted = false;
+
+    /**
+     * @ORM\Column(name="deletedReason", type="string", length=50, nullable=true)
+     */
+    private $deletedReason;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Platformd\SpoutletBundle\Entity\ContentReport", mappedBy="comment")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     * @ORM\OrderBy({"reportedAt" = "DESC"})
+     */
+    protected $contentReports;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Platformd\SpoutletBundle\Entity\CommentVote", mappedBy="comment")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
+    protected $votes;
+
+    public function __construct()
+    {
+        $this->replies          = new ArrayCollection();
+        $this->contentReports   = new ArrayCollection();
+        $this->votes            = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getParent()
+    {
+        return $this->parent;
+    }
+
+    public function setParent(Comment $parent)
+    {
+        $this->parent = $parent;
+
+        if (!$parent->getId()) {
+            throw new InvalidArgumentException('Parent comment must be persisted.');
+        }
+
+        $parentReplies = $parent->getReplies()->add($this);
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $author)
+    {
+        $this->author = $author;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getVotes()
+    {
+        return $this->votes;
+    }
+
+    public function getReplies()
+    {
+        return $this->replies;
+    }
+
+    public function setReplies($replies)
+    {
+        $this->replies = $replies;
+    }
+
+    public function setDeletedReason($value)
+    {
+
+        if ($value && !in_array($value, self::$validDeletedReasons)) {
+            throw new \InvalidArgumentException(sprintf('Invalid reason for deletion "%s" given', $value));
+        }
+
+        $this->deletedReason = $value;
+    }
+
+    public function getDeletedReason()
+    {
+        return $this->deletedReason;
+    }
+
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+
+    public function getContentReports()
+    {
+        return $this->contentReports;
+    }
+
+    public function setContentReports($value)
+    {
+        $this->contentReports = $value;
+    }
+
+    public function getContentType()
+    {
+        return "Comment";
+    }
+}
--- src/Platformd/SpoutletBundle/Entity/Thread.php
+++ src/Platformd/SpoutletBundle/Entity/Thread.php
@@ -4,42 +4,105 @@ namespace Platformd\SpoutletBundle\Entity;
 
 use Platformd\UserBundle\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Platformd\SpoutletBundle\Entity\Thread
  *
- * @ORM\Table(name="comment_thread")
+ * @ORM\Table(name="commenting_thread")
  * @ORM\Entity(repositoryClass="Platformd\SpoutletBundle\Entity\ThreadRepository")
  */
-class Thread 
+class Thread
 {
 	/**
-     * @Assert\NotNull(message="Required")
+     * @ORM\Id
      * @ORM\Column(type="string", length=255, unique=true)
      */
     protected $id;
 
-     /**
-     * Tells if new comments can be added in this thread
-     *
-     * @var bool
+    /**
+     * @ORM\Column(name="is_commentable", type="boolean")
      */
     protected $isCommentable = true;
 
     /**
-     * Denormalized date of the last comment
-     *
-     * @var DateTime
+     * @ORM\Column(name="last_commented_at", type="datetime")
      */
     protected $lastCommentAt = null;
 
     /**
-     * Url of the page where the thread lives
-     *
-     * @var string
+     * @ORM\Column(name="comment_count", type="integer")
+     */
+    protected $commentCount = 0;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     protected $permalink;
 
+    /**
+     * @ORM\OneToMany(targetEntity="Platformd\SpoutletBundle\Entity\Comment", mappedBy="thread")
+     * @ORM\JoinColumn(onDelete="SET NULL")
+     */
     protected $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getPermalink()
+    {
+        return $this->permalink;
+    }
+
+    public function setPermalink($permalink)
+    {
+        $this->permalink = $permalink;
+    }
+
+    public function isCommentable()
+    {
+        return $this->isCommentable;
+    }
+
+    public function setIsCommentable($isCommentable)
+    {
+        $this->isCommentable = (bool) $isCommentable;
+    }
+
+    public function getCommentCount()
+    {
+        return $this->commentCount;
+    }
+
+    public function setCommentCount($commentCount)
+    {
+        $this->commentCount = intval($commentCount);
+    }
+
+    public function incrementCommentCount($by = 1)
+    {
+        return $this->commentCount += intval($by);
+    }
+
+    public function getLastCommentAt()
+    {
+        return $this->lastCommentAt;
+    }
+
+    public function setLastCommentAt($lastCommentAt)
+    {
+        $this->lastCommentAt = $lastCommentAt;
+    }
 }
