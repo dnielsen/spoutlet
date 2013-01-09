f0800186371dc303ae9afc1224a95b52b8a5b0b0
playitcool <jefrykland@igamesinc.com>
2013-01-09 13:48:35 -0800

    Added ARP for submitting photo and getting photo featured

--- src/Platformd/SpoutletBundle/Controller/GalleryController.php
+++ src/Platformd/SpoutletBundle/Controller/GalleryController.php
@@ -10,6 +10,7 @@ use Platformd\SpoutletBundle\Form\Type\SubmitImageType;
 use Platformd\SpoutletBundle\Form\Type\GalleryChoiceType;
 use Platformd\SpoutletBundle\Form\Type\GalleryMediaType;
 use Platformd\MediaBundle\Form\Type\MediaType;
+use Platformd\CEVOBundle\Api\ApiException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Form\Form;
@@ -115,6 +116,14 @@ class GalleryController extends Controller
 
         $flashString = $media->getFeatured() ? 'Media was featured successfully!' : 'Media was unfeatured successfully!';
 
+        if($media->getFeatured()) {
+            try {
+                $response = $this->getCEVOApiManager()->GiveUserXp('photofeature', $media->getAuthor()->getCevoUserId());
+            } catch (ApiException $e) {
+
+            }
+        }
+
         $this->setFlash('success', $flashString);
         return $this->redirect($this->generateUrl('gallery_media_show', array( 'id' => $id )));
     }
@@ -163,6 +172,13 @@ class GalleryController extends Controller
             {
                 $published[] = $id;
                 $media->setPublished(true);
+
+                try {
+                    $response = $this->getCEVOApiManager()->GiveUserXp('photosubmit');
+                } catch (ApiException $e) {
+
+                }
+
             } else {
                 $unpublished[] = $id;
                 $allErrors[] = $errors;
@@ -757,5 +773,10 @@ class GalleryController extends Controller
     private function getCurrentUser()
     {
         return $this->get('security.context')->getToken()->getUser();
+    }
+
+    private function getCEVOApiManager()
+    {
+        return $this->get('pd.cevo.api.api_manager');
     }
 }
