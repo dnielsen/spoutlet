e1efbfb6b651f906b6789cffebde6eb6f31663f4
adfinlay <andy.finlay@gametheworld.com>
2013-03-04 20:16:07 +0000

    Updated deploy script to reflect new webservers

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,4 +1,4 @@
-set :stages, %w(production staging)
+set :stages, %w(production staging migration)
 set :stage_dir, "app/config/deploy"
 require 'capistrano/ext/multistage'
 
@@ -7,15 +7,15 @@ ssh_options[:port] = "22"
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
-set :app1, "ec2-174-129-86-236.compute-1.amazonaws.com"
-set :app2, "ec2-54-242-2-165.compute-1.amazonaws.com"
-set :app3, "ec2-23-20-100-179.compute-1.amazonaws.com"
-set :app4, "ec2-67-202-58-217.compute-1.amazonaws.com"
-set :app5, "ec2-54-234-107-140.compute-1.amazonaws.com"
-set :app6, "ec2-54-234-176-214.compute-1.amazonaws.com"
-set :app7, "ec2-50-16-102-100.compute-1.amazonaws.com"
-set :app8, "ec2-107-22-30-169.compute-1.amazonaws.com"
-set :app9, "ec2-54-234-184-156.compute-1.amazonaws.com"
+set :app1, "ec2-50-16-66-61.compute-1.amazonaws.com"
+set :app2, "ec2-54-224-27-105.compute-1.amazonaws.com"
+set :app3, "ec2-204-236-207-80.compute-1.amazonaws.com"
+set :app4, "ec2-107-22-71-108.compute-1.amazonaws.com"
+set :app5, "ec2-75-101-223-7.compute-1.amazonaws.com"
+set :app6, "ec2-174-129-62-95.compute-1.amazonaws.com"
+set :app7, "ec2-54-242-181-100.compute-1.amazonaws.com"
+set :app8, "ec2-50-16-75-123.compute-1.amazonaws.com"
+set :app9, "ec2-50-16-37-33.compute-1.amazonaws.com"
 
 set :repository,  "file:///Users/weaverryan/Sites/clients/spoutlet"
 
