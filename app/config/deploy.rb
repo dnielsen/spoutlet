8f1bc693f62548e01cf4dd4d48f002451f1691b0
Andrew Finlay <andy.finlay@gametheworld.com>
2013-03-05 14:24:18 +0000

    Added campsite testing server to deploy script

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,12 +1,13 @@
 set :stages, %w(production staging migration)
 set :stage_dir, "app/config/deploy"
 require 'capistrano/ext/multistage'
-
+logger.level = Logger::MAX_LEVEL
 # set the primary server, then use it to - potentially, have an array of servers
 ssh_options[:port] = "22"
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 
+# awa servers
 set :app1, "ec2-54-224-27-105.compute-1.amazonaws.com"
 set :app2, "ec2-50-16-66-61.compute-1.amazonaws.com"
 set :app3, "ec2-204-236-207-80.compute-1.amazonaws.com"
@@ -17,6 +18,9 @@ set :app7, "ec2-54-242-181-100.compute-1.amazonaws.com"
 set :app8, "ec2-50-16-75-123.compute-1.amazonaws.com"
 set :app9, "ec2-50-16-37-33.compute-1.amazonaws.com"
 
+# campsite servers
+set :app10, "ec2-54-235-26-82.compute-1.amazonaws.com"
+
 set :repository,  "file:///Users/weaverryan/Sites/clients/spoutlet"
 
 set :scm,         :git
@@ -25,8 +29,8 @@ set :user,        "ubuntu"
 # branch can be overridden in any of the "stage" files (e.g. staging)
 set :branch,      "master"
 
-role :web,        app1, app2, app3, app4, app5, app6, app7, app8, app9                         # Your HTTP server, Apache/etc
-role :app,        app1, app2, app3, app4, app5, app6, app7, app8, app9                         # This may be the same as your `Web` server
+role :web,        app1, app2, app3, app4, app5, app6, app7, app8, app9, app10                         # Your HTTP server, Apache/etc
+role :app,        app1, app2, app3, app4, app5, app6, app7, app8, app9, app10                        # This may be the same as your `Web` server
 role :db,         app1, :primary => true       # This is where Rails migrations will run
 
 set  :keep_releases,  3
