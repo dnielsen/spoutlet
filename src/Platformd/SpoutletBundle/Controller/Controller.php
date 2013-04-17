c7013e03d671ea569a28ec30c1bf121431ebecb5
Andrew Finlay <andy.finlay@gametheworld.com>
2013-04-17 11:37:44 +0000

    Removed redeclared function from base controller

--- src/Platformd/SpoutletBundle/Controller/Controller.php
+++ src/Platformd/SpoutletBundle/Controller/Controller.php
@@ -26,13 +26,6 @@ class Controller extends BaseController
         return $this->getDoctrine()->getEntityManager()->getRepository('SpoutletBundle:Country')->findOneByCode($code);
     }
 
-    protected function getCurrentCountry()
-    {
-        $ipAddress = $this->getRequest()->getClientIp(true);
-        $code = $this->getIpLookupUtil()->getCountryCode($ipAddress);
-        return $this->getDoctrine()->getEntityManager()->getRepository('SpoutletBundle:Country')->findOneByCode($code);
-    }
-
     /**
      * @return string
      */
