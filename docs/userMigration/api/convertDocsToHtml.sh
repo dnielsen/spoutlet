4ade22a339f2a0c7d5d6b5ca2440a97d4d1aaf75
InvertedAcceleration <invertedacceleration@gametheworld.com>
2013-05-29 02:03:48 +0000

    Updated docs

--- docs/userMigration/api/convertDocsToHtml.sh
+++ docs/userMigration/api/convertDocsToHtml.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc --smart --toc --standalone --include-in-header=bootstrap.min.css.include --highlight-style=pygments --to=html5 --output="userApi.html" "userApi.markdown"
-pandoc --smart --toc --standalone --include-in-header=bootstrap.min.css.include --highlight-style=pygments --to=html5 --output="userAvatar.html" "userAvatar.markdown"
+pandoc --smart --toc --standalone --include-in-header=bootstrap.min.css.include --highlight-style=pygments --to=html5 --output="AWA - Migration - API Information.html" "AWA - Migration - API Information.markdown"
+pandoc --smart --toc --standalone --include-in-header=bootstrap.min.css.include --highlight-style=pygments --to=html5 --output="AWA - Migration - Avatar Information.html" "AWA - Migration - Avatar Information.markdown"
