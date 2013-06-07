afa064cfacf7ce6c8940a13cc1fb98d0a8d63669
playitcool <jefrykland@igamesinc.com>
2013-06-08 00:59:31 +0100

    Added instagram icon and link for only latam.

--- src/Platformd/SpoutletBundle/Twig/SpoutletExtension.php
+++ src/Platformd/SpoutletBundle/Twig/SpoutletExtension.php
@@ -520,6 +520,7 @@ class SpoutletExtension extends Twig_Extension
             case 'CONTESTS_IMAGE':                   return $this->GetContestsLink($locale, 'image');
             case 'CONTESTS_GROUP':                   return $this->GetContestsLink($locale, 'group');
             case 'GIVEAWAYS':                       return $this->GetGiveawaysLink($locale);
+            case 'INSTAGRAM':                       return $this->GetInstagramLink($locale);
 
             default:
                 throw new \InvalidArgumentException(sprintf('Unknown link type "%s"', $linkType));
@@ -655,6 +656,16 @@ class SpoutletExtension extends Twig_Extension
             case 'ja':      return sprintf($format, 'Alienware_JP', $enAltText);
             case 'es':      return sprintf($format, 'AlienwareLatAm', $enAltText);
             case 'en_US':   return sprintf($format, $enLink, $enAltText);
+
+            default:        return false;
+        }
+    }
+
+    private function GetInstagramLink($locale) {
+        $link = '<a href="http://instagram.com/alienwarelatam/" target="_blank"><img src="/bundles/spoutlet/images/icons/icon-ig-14.png" alt="Instagram" /></a>';
+
+        switch ($locale) {
+            case 'es':      return $link;
 
             default:        return false;
         }
