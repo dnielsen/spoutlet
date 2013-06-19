a5f590acb3cbc77c5601d7cc1797e8c50bc40fe4
Andrew Finlay <andy.finlay@gametheworld.com>
2013-06-19 20:27:50 +0000

    Added metrics page for comments (see #AWA-875)
    Updated comment metrics to be date-inclusive
    
    Updated time setting for comment metrics to avoid null datetime issue

--- src/Platformd/SpoutletBundle/Entity/CommentRepository.php
+++ src/Platformd/SpoutletBundle/Entity/CommentRepository.php
@@ -129,5 +129,60 @@ class CommentRepository extends EntityRepository
             ->execute()
         ;
     }
+
+    public function getMetricsStats($from, $to)
+    {
+        $displayNames = array(
+            'deal_count' => 'Deal Comments',
+            'giveaway_count' => 'Giveaway Comments',
+            'group_count' => 'Group Comments',
+            'sweepstakes_count' => 'Sweepstakes Comments',
+            'gallery_image_count' => 'Gallery Image Comments',
+            'news_count' => 'News Comments',
+            'group_event_count' => 'Group Event Comments',
+            'global_event_count' => 'Event Comments',
+            'video_count' => 'Video Comments',
+            'group_discussion_post_count' => 'Group Discussion Posts',
+        );
+
+        $commentFrom = $from ? ' AND created_at >= "'.$from->setTime(0,0,0)->format('Y-m-d H:i:s').'"' : '';
+        $commentTo   = $to   ? ' AND created_at <= "'.$to->setTime(23,59,59)->format('Y-m-d H:i:s').'"'   : '';
+
+        $discussionFrom = $from ? ' AND created >= "'.$from->setTime(0,0,0)->format('Y-m-d H:i:s').'"' : '';
+        $discussionTo   = $to   ? ' AND created <= "'.$to->setTime(23,59,59)->format('Y-m-d H:i:s').'"'   : '';
+
+        $query = 'SELECT deal_comments.deal_count, group_comments.group_count, giveaway_comments.giveaway_count, sweepstakes_comments.sweepstakes_count,
+                        gallery_image_comments.gallery_image_count, news_comments.news_count, group_event_comments.group_event_count, global_event_comments.global_event_count,
+                        video_comments.video_count, group_discussion_posts.group_discussion_post_count
+                    FROM
+                    (SELECT count(*) deal_count FROM commenting_comment WHERE thread_id LIKE "deal-%"'.$commentFrom.$commentTo.') deal_comments,
+                    (SELECT count(*) giveaway_count FROM commenting_comment WHERE thread_id LIKE "giveaway-%"'.$commentFrom.$commentTo.') giveaway_comments,
+                    (SELECT count(*) group_count FROM commenting_comment WHERE thread_id LIKE "group-%"'.$commentFrom.$commentTo.') group_comments,
+                    (SELECT count(*) sweepstakes_count FROM commenting_comment WHERE thread_id LIKE "sweepstake-%"'.$commentFrom.$commentTo.') sweepstakes_comments,
+                    (SELECT count(*) gallery_image_count FROM commenting_comment WHERE thread_id LIKE "gallery_image-%"'.$commentFrom.$commentTo.') gallery_image_comments,
+                    (SELECT count(*) news_count FROM commenting_comment WHERE thread_id LIKE "news-%"'.$commentFrom.$commentTo.') news_comments,
+                    (SELECT count(*) group_event_count FROM commenting_comment WHERE thread_id LIKE "group-event-%"'.$commentFrom.$commentTo.') group_event_comments,
+                    (SELECT count(*) global_event_count FROM commenting_comment WHERE thread_id LIKE "global-event-%"'.$commentFrom.$commentTo.') global_event_comments,
+                    (SELECT count(*) video_count FROM commenting_comment WHERE thread_id LIKE "youtube-%"'.$commentFrom.$commentTo.') video_comments,
+                    (SELECT count(*) group_discussion_post_count FROM pd_group_discussion_post WHERE id > 0'.$discussionFrom.$discussionTo.') group_discussion_posts';
+
+
+        $stmt = $this->getEntityManager()
+            ->getConnection()
+            ->prepare($query);
+
+        $stmt->execute();
+        $commentData = $stmt->fetchAll();
+
+        $data = array();
+
+        foreach ($commentData[0] as $name => $count) {
+            if (isset($displayNames[$name])) {
+                $data[$displayNames[$name]] = $count;
+            }
+        }
+
+        return $data;
+    }
 }
 
