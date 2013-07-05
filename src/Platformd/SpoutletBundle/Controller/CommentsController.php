07d474cc7b07edf9433246565203cc046ecf4f38
playitcool <jefrykland@igamesinc.com>
2013-07-05 23:37:48 +0100

    Updated cache for comments to 30 seconds as well as giveaway flash and show messages

--- src/Platformd/SpoutletBundle/Controller/CommentsController.php
+++ src/Platformd/SpoutletBundle/Controller/CommentsController.php
@@ -233,7 +233,7 @@ class CommentsController extends Controller
             'permalink' => $thread->getPermalink(),
         ));
 
-        $this->varnishCache($response, 1);
+        $this->varnishCache($response, 30);
 
         return $response;
     }
@@ -289,7 +289,7 @@ class CommentsController extends Controller
             'permalink' => $thread->getPermalink(),
         ));
 
-        $this->varnishCache($response, 1);
+        $this->varnishCache($response, 30);
 
         return $response;
     }
@@ -333,7 +333,7 @@ class CommentsController extends Controller
             'permalink' => $thread->getPermalink(),
         ));
 
-        $this->varnishCache($response, 1);
+        $this->varnishCache($response, 30);
 
         return $response;
     }
