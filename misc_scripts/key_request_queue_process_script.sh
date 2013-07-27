205d77b787a24ef6a41a35d55bc63f17d732b4c1
Andrew Finlay <andy.finlay@gametheworld.com>
2013-07-27 12:16:14 +0000

    Updated key request flocking script

--- misc_scripts/key_request_queue_process_script.sh
+++ misc_scripts/key_request_queue_process_script.sh
@@ -9,10 +9,10 @@ end_script()
 
 trap end_script SIGTERM
 
-exec 200>/var/www/alienwarearena/deploy/current/misc_scripts/flock_files/key_request_queue_process_script
+exec 200>./shared/misc_scripts/flock_files/key_request_queue_process_script
 flock -n 200 || exit 1
 
 while [ $EXIT -eq 0 ]; do
-    /var/www/alienwarearena/deploy/current/app/console pd:keyRequestQueue:process -e prod
+    ./current/app/console pd:keyRequestQueue:process -e prod
     sleep 2
 done
