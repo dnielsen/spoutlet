5c4895b0c0ce7e5943299a76d64e06fe86b72c0a
Osman Dandia <osman@platformd.com>
2013-09-20 04:45:50 +0000

    Adding package install script (taken from platformd_vagrant repo)

---
+++ install_packages.sh
@@ -0,0 +1,23 @@
+echo "Installing Additional Packages"
+echo "---------------------------------------"
+echo
+
+sudo apt-get install curl
+
+curl http://repo.varnish-cache.org/debian/GPG-key.txt | sudo apt-key add -
+
+if ! grep -q 'deb http://repo.varnish-cache.org/ubuntu/ precise varnish-3.0' /etc/apt/sources.list ;
+then
+echo "deb http://repo.varnish-cache.org/ubuntu/ precise varnish-3.0" | sudo tee -a /etc/apt/sources.list
+fi
+
+sudo apt-get update
+
+sudo apt-get -q -y install varnish htop screen vim apache2-doc git-core libapache2-mod-php5 php5-intl php-apc php5-curl php5-gd php5-suhosin php5-mysql php5-mcrypt memcached php5-memcache php5-memcached php5-sqlite ftp-upload ncurses-term php5-xdebug mysql-server mysql-client
+
+echo "---------------------------------------"
+echo "Package installation complete"
+echo "---------------------------------------"
+
+echo
+
