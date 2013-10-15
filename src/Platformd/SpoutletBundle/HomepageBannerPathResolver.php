096db1d764101182340cbc84cdbee474ccb9d247
sandip limbachiya <sandip2451984@gmail.com>
2013-10-15 05:46:48 +0000

    add remaining file

--- src/Platformd/SpoutletBundle/HomepageBannerPathResolver.php
+++ src/Platformd/SpoutletBundle/HomepageBannerPathResolver.php
@@ -4,7 +4,6 @@ namespace Platformd\SpoutletBundle;
 
 use Platformd\SpoutletBundle\PathResolver;
 use Platformd\SpoutletBundle\Entity\HomepageBanner;
-
 use Gaufrette\Adapter\AmazonS3;
 
 /**
@@ -12,16 +11,27 @@ use Gaufrette\Adapter\AmazonS3;
 */
 class HomepageBannerPathResolver extends PathResolver
 {
-
+ 
+ 
   /**
    * {@inheritDoc}
    */
   public function getPath($banner, array $options)
   {
+     //    $this->customfn();	 
+    
     $path = isset($options['size']) && $options['size'] === 'banner' ? $banner->getBanner() : $banner->getThumb();
+   
     if ($this->filesystem->getAdapter() instanceof AmazonS3) {
-
-        $cf = 'http://'.$this->bucketName.'.s3.amazonaws.com';
+      
+        if ($this->bucketName == 'platformd') {
+            $cf = "http://media.alienwarearena.com";
+        } else {
+	    if($this->objectStorage == "HpObjectStorage")
+              $cf = "https://region-a.geo-1.objects.hpcloudsvc.com/v1/10873218563681/platformd-public";
+            else
+              $cf = "https://s3.amazonaws.com/platformd-public";
+        }
 
         return sprintf('%s%s/%s', $cf, $this->prefix, $path);
     }
