06d672c7055f63f8cba3a2d4c383f1910bb125bb
sandip limbachiya <sandip2451984@gmail.com>
2013-10-31 10:39:31 +0000

    Add SendGrid Functionality and make it configurable with AWS SQS Service

--- app/autoload.php
+++ app/autoload.php
@@ -36,7 +36,8 @@ $loader->registerNamespaces(array(
     'Cybernox'                      => __DIR__.'/../vendor/bundles',
     'Exercise'                      => __DIR__. '/../vendor/bundles',
     'Vich'                          => __DIR__.'/../vendor/bundles',
-    'HPCloud'                       => __DIR__.'/../vendor/HpCloudPhp/src' 
+    'HPCloud'                       => __DIR__.'/../vendor/HpCloudPhp/src',
+    'SendGrid'	                    => __DIR__.'/../vendor/sendgrid-php'	 
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
