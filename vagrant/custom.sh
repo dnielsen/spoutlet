80ee85b06ece150995b04bce72c89900b22a9a37
Eric Price <eric@platformd.com>
2013-11-17 23:48:03 -0800

    Various fixes to the vagrant script, still no working login

--- vagrant/custom.sh
+++ vagrant/custom.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# DB_NAME=campsite
-# DB_USER=root
-# DB_PASS=platformd
 HOME="/home/vagrant"
 PROJ="$HOME/campsite"
+HOST="campsite"
+
+DB_NAME="campsite"
+DB_USER="root"
+DB_PASS="platformd"
 
 echo
 echo "---------------------------------------"
@@ -15,23 +17,17 @@ sudo chmod 600 /home/vagrant/.ssh/id_rsa
 #sudo curl -k https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub >> /home/vagrant/.ssh/authorized_keys
 echo "---------------------------------------"
 
-if [ -f /home/vagrant/user_specific.sh ]; then
-    echo
-    echo "---------------------------------------"
-    echo "Running User Specific Script"
-    /home/vagrant/user_specific.sh
-    echo "---------------------------------------"
-fi
-
 echo
 echo "---------------------------------------"
-echo "Setting Locale"
+echo "Setting Locale & timezone"
 sudo tee /etc/default/locale <<EOF > /dev/null
 LANG="en_US.UTF-8"
 LANGUAGE="en_US:en"
 EOF
+echo \"America/Los_Angeles\" | sudo tee /etc/timezone && dpkg-reconfigure --frontend noninteractive tzdata
 echo "---------------------------------------"
 echo
+
 echo
 echo "---------------------------------------"
 echo "Updating System Clock"
@@ -43,7 +39,7 @@ echo
 echo "---------------------------------------"
 echo "Install Applications"
 sudo apt-get update
-sudo apt-get -q -y install varnish curl htop screen vim apache2-doc git-core libapache2-mod-php5 php5-intl php-apc php5-curl php5-gd php5-mysql php5-mcrypt memcached php5-sqlite php5-memcache ftp-upload ncurses-term php5-memcached php5-xdebug
+sudo apt-get -q -y install varnish htop screen vim acl apache2-doc git-core libapache2-mod-php5 php5-intl php-apc php5-curl php5-gd php5-mysql php5-mcrypt memcached php5-memcache php5-memcached php5-sqlite ftp-upload ncurses-term php5-xdebug mysql-client php-pear
 echo "---------------------------------------"
 echo
 
@@ -69,21 +65,53 @@ else
   echo "---------------------------------------"
 fi
 
-if [ ! "$( ls -A $HOME/app_resources )" ]; then
-     echo "app_resources appears to be empy, refreshing content..."
-     sudo -u vagrant -g vagrant cp -r $PROJ/app/Resources/* $HOME/app_resources 
+echo
+if [ ! -f /etc/apache2/sites-available/$HOST ]; then
+  echo "---------------------------------------"
+  echo "Configuring Apache2 VirtualHost"
+  sudo tee /etc/apache2/sites-available/$HOST <<EOF > /dev/null
+    <VirtualHost *:80>
+      ServerAdmin webmaster@dummy-host.example.com
+
+      ServerName      $HOST
+      DocumentRoot    ${PROJ}/web
+      ErrorLog        ${PROJ}/logs/error.log
+      CustomLog       ${PROJ}/logs/access.log common
+
+      DirectoryIndex app_dev.php
+
+      <Directory ${PROJ}/web>
+        Options FollowSymLinks
+        AllowOverride All
+      </Directory>
+    </VirtualHost>
+EOF
+  echo "---------------------------------------"
+else 
+  echo "---------------------------------------"
+  echo "Skipping Apache2 VirtualHost file"
+  echo "---------------------------------------"
 fi
 
+a2dissite default
+a2enmod rewrite
+a2ensite ${HOST}
+service apache2 restart
 echo
-echo "---------------------------------------"
-echo "Configure parameters.ini"
-sudo -u vagrant -g vagrant cp parameters.ini $PROJ/app/config/parameters.ini
-# sudo -u vagrant -g vagrant cp app/config/parameters.ini.dist app/config/parameters.ini
-# sudo -u vagrant -g vagrant sed -i "s/\(database_name *= *\).*/\1$DB_NAME/" app/config/parameters.ini
-# sudo -u vagrant -g vagrant sed -i "s/\(database_user *= *\).*/\1$DB_USER/" app/config/parameters.ini
-# sudo -u vagrant -g vagrant sed -i "s/\(database_password*= *\).*/\1$DB_PASSWORD/" app/config/parameters.ini
-echo "---------------------------------------"
-echo
+
+if [ ! -f $PROJ/app/config/parameters.ini ]; then
+  echo "---------------------------------------"
+  echo "Adding parameters.ini from user_data"
+  sudo -u vagrant -g vagrant cp /vagrant/user_data/parameters.ini $PROJ/app/config/parameters.ini
+  sudo -u vagrant -g vagrant sed -i "s/\(database_name *= *\).*/\1$DB_NAME/" $PROJ/app/config/parameters.ini
+  sudo -u vagrant -g vagrant sed -i "s/\(database_user *= *\).*/\1$DB_USER/" $PROJ/app/config/parameters.ini
+  sudo -u vagrant -g vagrant sed -i "s/\(database_password*= *\).*/\1$DB_PASSWORD/" $PROJ/app/config/parameters.ini
+  echo "---------------------------------------"
+else 
+  echo "---------------------------------------"
+  echo "Skipping parameteres.ini install -- already exists"
+  echo "---------------------------------------"
+fi
 
 echo
 echo "---------------------------------------"
@@ -92,31 +120,34 @@ sudo -u vagrant -g vagrant php $PROJ/bin/vendors install
 echo "---------------------------------------"
 echo
 
+OLD_DIR=`pwd`;echo $PWD
+cd $PROJ
+
+echo "---------------------------------------"
+echo "Create database and run migrations"
+
+php app/console doc:data:create
+php app/console doc:mig:mig --no-interaction
+mysql -u$DB_USER -p$DB_PASS $DB_NAME < /vagrant/user_data/campsite_sites.sql
+echo "---------------------------------------"
+
 if [ "$( ls $HOME/app_resources )" ]; then 
   echo "---------------------------------------"
-  echo "Copy themes changes from synced directory"
-  cp -r $HOME/app_resources/* $PROJ/app/Resources; 
+  echo "Copying in themes changes from synced directory "
+  sudo -u vagrant -g vagrant cp -r $HOME/app_resources/* $PROJ/app/Resources; 
   echo "---------------------------------------"
 else
   echo "---------------------------------------"
-  echo "Themes changes from synced directory skipped -- no files"
+  echo "No themes changes from synced directory, refreshing content..."
+  sudo -u vagrant -g vagrant cp -r $PROJ/app/Resources/* $HOME/app_resources
   echo "---------------------------------------"
 fi;
 
-cd $PROJ && source $PROJ/updateThemes.sh
+sudo -u vagrant -g vagrant $PROJ/updateThemes.sh
+cd $OLD_DIR
 
-# php app/console doc:data:create
-# php app/console doc:mig:mig --no-interaction
-
-# php app/console themes:install web --symlink
-
-# cp /home/vagrant/app_resources ./app/Resources
-
-# php app/console assetic:dump --env=prod --no-debug
-# php app/console assets:install web --symlink
-
-# php app/console cache:clear --env=prod --no-debug
-
-#cp /vagrant/setup_environment.sh /home/vagrant/
-#sudo chown -R vagrant:vagrant /home/vagrant/
-
+echo "---------------------------------------"
+echo "Refresh the cache"
+rm -rf $PROJ/app/cache/*
+sudo -u vagrant -g vagrant php $PROJ/app/console cache:clear --env=dev 
+echo "---------------------------------------"
