d2b60ab6a7b0ac788bdc5d2b4785dbe9997a7032
Eric Price <eric@platformd.com>
2014-01-04 16:30:22 -0800

    Updated feedback table names

--- misc_scripts/updateFeedbackTables.sh
+++ misc_scripts/updateFeedbackTables.sh
@@ -16,40 +16,40 @@ DB_HOST="localhost"
 SITE_NAME="Campsite"
 
 values=(\
-'accounts_groups' 'My Group''s Feedback' \
-'accounts_events' 'My Event''s Feedback' \
-'default_index' 'Front Page Feedback' \
-'about' 'About Page Feedback' \
-'contact' 'Contact Page Feedback' \
-'groups' 'All Groups Feedback' \
-'global_events_index' 'All Events Feedback'  \
-'profile' 'Profile Feedback' \
-'accounts_settings' 'Profile Settings Feedback'  \
-'entry_set_view' 'List View Feedback'  \
-'profile_edit' 'Profile Edit Feedback'  \
-'group_event_contact' 'Event Contact Feedback'  \
-'group_event_attendees' 'Event Attendees Feedback'  \
-'group_show' 'View Group Feedback' \
-'group_event_view' 'View Event Feedback'  \
-'group_new' 'New Group Feedback' \
-'group_edit' 'Edit Group Feedback' \
-'entry_set_new' 'New List Feedback' \
-'idea_admin_event' 'New Event Feedback' \
-'idea_admin' 'Event Admin Feedback' \
-'idea_admin_event' 'Edit Event Feedback' \
-'idea_admin_images' 'Add Event Images Feedback' \
-'idea_admin_member_approvals' 'Approve Event Members Feedback' \
-'idea_admin_criteria_all' 'Show Event Criteria Feedback' \
-'idea_admin_criteria' 'New Event Criteria Feedback' \
-'idea_admin_criteria_get' 'Edit Event Criteria Feedback' \
-'idea_summary' 'Event Round Summery Feedback' \
-'idea_create_form' 'New Entry Feedback' \
-'idea_show' 'Show Entry Feedback' \
-'idea_edit_form' 'Edit Entry Feedback' \
-'idea_upload_form' 'Entry Image Upload Feedback' \
-'idea_add_link_form' 'Entry Add Link Feedback' \
-'fos_user_security_login' 'Login Page Feedback' \
-'fos_user_resetting_request' 'Forgot Password Feedback' \
+'accounts_groups' 'Feedback for My Group page' \
+'accounts_events' 'Feedback for My Event page' \
+'default_index' 'Feedback for Front page' \
+'about' 'Feedback for About page' \
+'contact' 'Feedback for Contact page' \
+'groups' 'Feedback for All Groups page' \
+'global_events_index' 'Feedback for All Events page'  \
+'profile' 'Feedback for Profile page' \
+'accounts_settings' 'Feedback for Profile Settings page'  \
+'entry_set_view' 'Feedback for List View page'  \
+'profile_edit' 'Feedback for Profile Edit page'  \
+'group_event_contact' 'Feedback for Event Contact page'  \
+'group_event_attendees' 'Feedback for Event Attendees page'  \
+'group_show' 'Feedback for View Group page' \
+'group_event_view' 'Feedback for View Event page'  \
+'group_new' 'Feedback for New Group page' \
+'group_edit' 'Feedback for Edit Group page' \
+'entry_set_new' 'Feedback for New List page' \
+'idea_admin_event' 'Feedback for New Event page' \
+'idea_admin' 'Feedback for Event Admin page' \
+'idea_admin_event' 'Feedback for Edit Event page' \
+'idea_admin_images' 'Feedback for Add Event Images page' \
+'idea_admin_member_approvals' 'Feedback for Approve Event Members page' \
+'idea_admin_criteria_all' 'Feedback for Show Event Criteria page' \
+'idea_admin_criteria' 'Feedback for New Event Criteria page' \
+'idea_admin_criteria_get' 'Feedback for Edit Event Criteria page' \
+'idea_summary' 'Feedback for Event Round Summery page' \
+'idea_create_form' 'Feedback for New Entry page' \
+'idea_show' 'Feedback for Show Entry page' \
+'idea_edit_form' 'Feedback for Edit Entry page' \
+'idea_upload_form' 'Feedback for Entry Image Upload page' \
+'idea_add_link_form' 'Feedback for Entry Add Link page' \
+'fos_user_security_login' 'Feedback for Login page' \
+'fos_user_resetting_request' 'Feedback for Forgot Password' \
 );
 
 size=${#values[@]};
