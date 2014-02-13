5cdb1caec942c3b2737cb920bdb4ab3ce2d97bac
sandip limbachiya <sandip2451984@gmail.com>
2014-02-13 05:03:57 +0000

    Change uuidgen implmentation

--- src/Platformd/UserBundle/Entity/UserManager.php
+++ src/Platformd/UserBundle/Entity/UserManager.php
@@ -306,11 +306,10 @@ class UserManager extends BaseUserManager
 
     private function uuidGen()
     {
-      //$html = $this->getHTML("http://www.famkruithof.net/uuid/uuidgen",10);
-      $html = file_get_contents("http://www.famkruithof.net/uuid/uuidgen");
-      preg_match("/<h3>(.*)<\/h3>/i", $html, $match);
-      $title = $match[1];
-      return $title;
+       $data = openssl_random_pseudo_bytes(16);
+       $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 000
+       $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+       return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     } 
 
     public function addLoginRecord($user, $request)
--- src/Platformd/UserBundle/Model/GallaryManager.php
+++ src/Platformd/UserBundle/Model/GallaryManager.php
@@ -128,8 +128,10 @@ class GallaryManager
 
     protected function uuidGen()
     {
-        return str_replace("\n", '', `uuidgen -r`);
-      // return rand(5, 15);
+         $data = openssl_random_pseudo_bytes(16);
+         $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0010
+         $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+         return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     public function getSignedImageUrl($gallaryUuid, $filename, User $user)
