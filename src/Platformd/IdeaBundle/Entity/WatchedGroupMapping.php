2c04c7b594bbc672b634efea40bcd9d624efe24d
Osman Dandia <odandia@gmail.com>
2014-07-07 12:01:22 -0700

    Add WatchedGroupMapping entity

---
+++ src/Platformd/IdeaBundle/Entity/WatchedGroupMapping.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Platformd\IdeaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Platformd\GroupBundle\Entity\Group;
+
+/**
+ * WatchedGroupMapping
+ * @ORM\Table(name="watched_groups")
+ * @ORM\Entity
+ */
+class WatchedGroupMapping
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platformd\UserBundle\Entity\User", inversedBy="watchedGroups")
+     */
+    protected $user;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Platformd\GroupBundle\Entity\Group")
+     */
+    protected $group;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * Constructor
+     */
+    public function __construct($user, $event)
+    {
+        $this->user = $user;
+        $this->group = $group;
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return datetime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set owner
+     *
+     * @param Platformd\UserBundle\Entity\User $owner
+     */
+    public function setUser(\Platformd\UserBundle\Entity\User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get owner
+     *
+     * @return Platformd\UserBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set group
+     *
+     * @param Platformd\GroupBundle\Entity\Group $group
+     */
+    public function setGroup(\Platformd\GroupBundle\Entity\Group $group)
+    {
+        $this->group = $group;
+    }
+
+    /**
+     * Get group
+     *
+     * @return Platformd\GroupBundle\Entity\Group
+     */
+    public function getGroup()
+    {
+        return $this->group;
+    }
+
+}
