5b80366eff58a69fde8db94d8e6ca8a949cf0de0
gimballock <eric@platformd.com>
2014-07-30 17:24:37 -0700

    remove themes commands from updateThemes.sh and deploy.sh

--- update.sh
+++ update.sh
@@ -19,10 +19,10 @@ echo 'Installing assets'
 echo
 php app/console assets:install web --symlink
 
-echo '============================='
-echo 'Installing themes'
-echo
-php app/console themes:install web --symlink
+# echo '============================='
+# echo 'Installing themes'
+# echo
+# php app/console themes:install web --symlink
 
 echo '============================='
 echo 'Assetic Dump'
