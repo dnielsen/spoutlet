742523c15d38ddd7ea9a81e09bc88a8551549e7d
gimballock <eric@platformd.com>
2014-08-18 13:24:42 -0700

    Added single sponsorships resource to api

--- node-api/routes/sponsorships.js
+++ node-api/routes/sponsorships.js
@@ -78,9 +78,9 @@ exports.find_all = function (req, resp, next) {
     return resource.find_all(req, resp, next, collection_handler);
 };
 
-// exports.find_by_primary_key = function (req, resp, next) {
-//     return resource.find_by_primary_key(req, resp, next);
-// };
+exports.find_by_primary_key = function (req, resp, next) {
+    return resource.find_by_primary_key(req, resp, next);
+};
 
 // exports.create = function (req, resp, next) {
 //     return resource.create(req, resp, next);
