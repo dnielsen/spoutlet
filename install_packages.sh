4ce8720a9faed5e8fa62e9ede1f0826472fe24bc
Osman Dandia <odandia@gmail.com>
2014-09-10 17:29:33 -0700

    Add nodejs to install packages script

--- install_packages.sh
+++ install_packages.sh
@@ -16,9 +16,10 @@ echo "deb http://repo.varnish-cache.org/ubuntu/ precise varnish-3.0" | sudo tee 
 fi
 
 sudo add-apt-repository ppa:ondrej/php5-oldstable -y
+sudo apt-add-repository ppa:chris-lea/node.js -y
 sudo apt-get update
 
-sudo apt-get -q -y install varnish htop screen vim acl apache2-doc git-core libapache2-mod-php5 php5-intl php-apc php5-curl php5-gd php5-mysql php5-mcrypt memcached php5-memcache php5-memcached php5-sqlite ftp-upload ncurses-term php5-xdebug mysql-server mysql-client php-pear
+sudo apt-get -q -y install varnish htop screen vim acl apache2-doc git-core libapache2-mod-php5 php5-intl php-apc php5-curl php5-gd php5-mysql php5-mcrypt memcached php5-memcache php5-memcached php5-sqlite ftp-upload ncurses-term php5-xdebug mysql-server mysql-client php-pear python-software-properties nodejs
 
 echo
 echo "---------------------------------------"
